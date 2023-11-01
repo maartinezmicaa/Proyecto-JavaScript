467c53415e3105bc37aad7a4de879900b102f0e5
Micaela Martinez <maartinezmicaa@hotmail.com>
2023-11-01 10:20:22 -0300

    Primer proyecto js

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+alert ("Bienvenido, somos Bombonday Vinos! Te gustaria ingresar?")
+
+alert ("¿Eres mayor de edad?")
+    let edad = prompt ("Ingrese edad")
+    if (edad >= 18){
+        alert("Muy bien! Eres Bienvenido!")
+    
+
+for (let i=1 ; i<=4 ; i++){
+
+function multiplicacion (vino1, costo1){
+    let vino =prompt("Que vino te gustaria comprar?")
+    
+    if(vino == "tinto"){
+        vino1 = parseInt= "2000"
+        costo1 = prompt("cuantos quieres?")
+    let resultado = vino1*costo1
+    alert ("El "+vino+" te sale "+resultado+"")
+    }else if(vino == "blanco"){
+        vino1 = parseInt= "1700"
+        costo1 = prompt("cuantos quieres?")
+    let resultado = vino1*costo1
+    alert ("El "+vino+" te sale "+resultado+"")
+    }else if(vino == "extra brutt"){
+        vino1 = parseInt= "2300"
+        costo1 = prompt("cuantos quieres?")
+    let resultado = vino1*costo1
+    alert ("El "+vino+" te sale "+resultado+"")
+    }else{
+        alert("No tenemos ese vino")
+    }
+    
+    
+    
+}
+multiplicacion()
+
+prompt ("quieres seguir comprando?")
+
+
+}
+}else{
+    alert("No puedes comprar alcohol")
+}
+
+
